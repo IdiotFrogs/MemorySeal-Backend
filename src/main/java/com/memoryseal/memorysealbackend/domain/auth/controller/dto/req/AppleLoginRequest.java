71b77e323b7759244e08ae3fd181a68b8beae2e7
Josh0511 <wh5054@naver.com>
2026-03-31 09:26:29 +0900

    refactor: authorizationCode nullable 변경을 통한 안드로이드 대응

--- src/main/java/com/memoryseal/memorysealbackend/domain/auth/controller/dto/req/AppleLoginRequest.java
+++ src/main/java/com/memoryseal/memorysealbackend/domain/auth/controller/dto/req/AppleLoginRequest.java
@@ -1,5 +1,6 @@
 package com.memoryseal.memorysealbackend.domain.auth.controller.dto.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,5 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AppleLoginRequest {
     private String idToken;
+    @Schema(description = "authorizationCode", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String authorizationCode;
 }
