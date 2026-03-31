847389d0655e67c5fb49f3eddf936ddcda842e62
Josh0511 <wh5054@naver.com>
2026-03-31 18:51:11 +0900

    refactor: 공동 작업자 리스트 조회 에러 핸들링

--- src/main/java/com/memoryseal/memorysealbackend/domain/contributor/controller/ContributorController.java
+++ src/main/java/com/memoryseal/memorysealbackend/domain/contributor/controller/ContributorController.java
@@ -29,14 +29,23 @@ public class ContributorController {
 
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "성공"),
-            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음",
+            @ApiResponse(responseCode = "401", description = "로그인 필요",
                     content = @Content(schema = @Schema(implementation = ErrorResponse.class),
-                    examples = @ExampleObject(value = "{\"status\": \"404\", \"error\": \"USER_NOT_FOUND\", \"message\": \"존재하지 않는 사용자 입니다.\", \"path\": \"/time-capsules/{capsuleId}/collaborators\"}")))
+                    examples = @ExampleObject(name = "로그인 필요", value = "{\"status\": \"401\", \"error\": \"NEED_LOGIN\", \"message\": \"로그인이 필요합니다.\", \"path\": \"/time-capsules/{capsuleId}/collaborators\"}"))),
+            @ApiResponse(responseCode = "403", description = "접근 권한 없음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = @ExampleObject(name = "접근 권한 없음", value = "{\"status\": \"403\", \"error\": \"ACCESS_DENIED\", \"message\": \"해당 요청을 처리할 권한이 없습니다.\", \"path\": \"/time-capsules/{capsuleId}/collaborators\"}"))),
+            @ApiResponse(responseCode = "404", description = "1. 사용자를 찾을 수 없음 \t\n 2. 타임캡슐을 찾을 수 없음",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class),
+                    examples = {
+                            @ExampleObject(value = "{\"status\": \"404\", \"error\": \"USER_NOT_FOUND\", \"message\": \"존재하지 않는 사용자 입니다.\", \"path\": \"/time-capsules/{capsuleId}/collaborators\"}"),
+                            @ExampleObject(value = "{\"status\": \"404\", \"error\": \"TIMECAPSULE_NOT_FOUND\", \"message\": \"타임캡슐을 찾을 수 없습니다.\", \"path\": \"/time-capsules/{capsuleId}/collaborators\"}")
+                    }))
     })
     @GetMapping("/{capsuleId}/collaborators")
     @Operation(summary = "공동 작업자 리스트 조회")
     public List<ContributorResponseDto> getDetail(
-            @Parameter(description = "타임캡슐의 ID", required = true)
+            @Parameter(description = "타임캡슐 ID", required = true)
             @PathVariable Long capsuleId) {
         return contributorService.getDetail(capsuleId);
     }
