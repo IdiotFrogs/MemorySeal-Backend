6d35fb87d2ce165328f8f2fb064993a9a94c000e
Josh0511 <wh5054@naver.com>
2026-08-11 19:37:04 +0900

    fix: BuryRequestDto validation 추가를 통한 NPE 방지

--- src/main/java/com/memoryseal/memorysealbackend/domain/contributor/controller/dto/req/BuryRequestDto.java
+++ src/main/java/com/memoryseal/memorysealbackend/domain/contributor/controller/dto/req/BuryRequestDto.java
@@ -2,6 +2,7 @@ package com.memoryseal.memorysealbackend.domain.contributor.controller.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 
 @Getter
 public class BuryRequestDto {
+    @NotNull(message = "타임캡슐 개봉 날짜는 필수 항목 입니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     @Schema(description = "타임캡슐 열리는 날짜")
